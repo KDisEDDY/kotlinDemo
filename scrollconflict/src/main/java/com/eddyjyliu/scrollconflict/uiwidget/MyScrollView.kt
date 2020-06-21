dfee8996da5fbe65cb62e3c1ad5703a54111843e
eddyjyliu <eddyjyliu@tencent.com>
2020-06-21 13:02:10 +0800

    step---构建了一个scrollView 嵌套scrollView的滑动冲突的例子

---
+++ scrollconflict/src/main/java/com/eddyjyliu/scrollconflict/uiwidget/MyScrollView.kt
@@ -0,0 +1,13 @@
+package com.eddyjyliu.scrollconflict.uiwidget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.widget.ScrollView
+
+/**
+ * 外部拦截法
+ */
+class MyScrollView @JvmOverloads constructor(context: Context?, attrs: AttributeSet? = null, defStyleAttr: Int = 0):
+        ScrollView(context, attrs, defStyleAttr) {
+}
