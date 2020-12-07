8fabfee162a653d58b15b999ae0c89dea3736039
eddyjyliu <eddyjyliu@tencent.com>
2020-12-07 22:43:10 +0800

    去掉滑动冲突的module，添加test用的gridLayoutActivity

---
+++ app/src/main/java/project/ljy/kotlindemo/ui/GridLayoutActivity.kt
@@ -0,0 +1,34 @@
+package project.ljy.kotlindemo.ui
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.GridLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import project.ljy.kotlindemo.R
+import project.ljy.kotlindemo.adapter.GridAdapter
+
+
+class GridLayoutActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grid_layout)
+
+        var startTimeMill = System.currentTimeMillis()
+        val gridLayout = findViewById<GridLayout>(R.id.grid_layout)
+        Log.d("GridLayoutActivity", "inflate gridLayout cost:${System.currentTimeMillis() - startTimeMill}")
+
+        //测试recyclerView的绘制耗时
+        val dataList = arrayListOf("1", "2", "3", "4", "5", "6")
+        val gridRecyclerView = findViewById<RecyclerView>(R.id.grid_layout2)
+        startTimeMill = System.currentTimeMillis()
+        gridRecyclerView.adapter = GridAdapter(this, dataList)
+        gridRecyclerView.layoutManager = GridLayoutManager(this, 4)
+        Log.d("GridLayoutActivity", "inflate grid recyclerView cost:${System.currentTimeMillis() - startTimeMill}")
+    }
+
+
+}
