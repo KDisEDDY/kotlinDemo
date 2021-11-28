8d27a1ed38275df7c504c32dabc6809594cb6257
KDisEDDY <LJY68835441@163.com>
2021-11-29 00:31:16 +0800

    --taskID=20211129 增加mediaController

--- videoedit/src/main/java/com/eddyjyliu/videoedit/PreviewActivity.kt
+++ videoedit/src/main/java/com/eddyjyliu/videoedit/PreviewActivity.kt
@@ -3,10 +3,12 @@ package com.eddyjyliu.videoedit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.SurfaceView
+import com.eddyjyliu.videoedit.controller.MediaController
 
 class PreviewActivity : AppCompatActivity() {
 
     lateinit var previewSurface: SurfaceView
+    val mediaController by lazy { MediaController() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
